6c14cd0f0e39abe02078464141458eb031c78d9e
nkmodsbot <117563049+nkmodsbot@users.noreply.github.com>
2023-05-30 17:33:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,43 @@
+echo Hey Vroo..
+sleep 3
+clear
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Your Device Gonna heckk...
+sleep 1
+echo Yesh Its me NKMODS
+sleep 2
+clear
+echo Hehe.. vro fear ayoo?
+sleep 3
+clear 
+echo Dont Worry Vroo , Now Gonna im gonna setup the rat.
+sleep 1
+echo wait...
+sleep 2
+echo Started work..
+sleep 2
 clear
 echo 🗿STARTING THE SETUP...
 sleep 3
-echo Yesh Its Me NKMODS!..
-sleep 2
+
+
 
 pkg update && upgrade -y
 clear
